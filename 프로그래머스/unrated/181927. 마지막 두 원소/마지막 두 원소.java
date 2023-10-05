a8ada236013b623a1161b8317bb093e1d78a5d4c
junheiLee <116637090+junheiLee@users.noreply.github.com>
2023-10-05 12:16:38 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -5,14 +5,14 @@ class Solution {
         int length = num_list.length;
         int[] answer = new int[length + 1];
         
-        int lastNum = num_list[length-1];
-        int laterNum = num_list[length-2];
+        int last = num_list[length-1];
+        int before = num_list[length-2];
         int addNum = 0;
         
-        if(laterNum < lastNum) {
-            addNum = lastNum - laterNum;
+        if(before < last) {
+            addNum = last - before;
         } else {
-            addNum = lastNum * 2;
+            addNum = last * 2;
         }
         
         System.arraycopy(num_list, 0, answer, 0, length);
